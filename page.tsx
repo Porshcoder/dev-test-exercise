ee9b03799e599b9113201e8b7537edb249e88fd5
Porshcoder <nditigana01@gmail.com>
2023-06-18 23:09:28 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Validate  from "./validate"
+
+export default function Home() {
+  return (
+    <>
+
+     <h2 className=" display-3 text-center mt-5 text-black">Next Js - Login Validation</h2>
+      <Validate/>
+    </>
+  )
+}
